9e2a8d85b1e59392d50ebfa6fc603636def8139f
Ivan Dapena <ivan.dapena@solera.com>
2022-10-04 13:39:03 +0200

    ULTIMOS CAMBIOS

--- questionnaire/my-app/src/App.js
+++ questionnaire/my-app/src/App.js
@@ -5,6 +5,7 @@ function App() {
 
   const [preguntas,setPreguntas]=React.useState()
   const [preguntaIndex, setPreguntaIndex] = React.useState(0)
+  const [respuestas, setRespuestas]=React.useState([])
 
   React.useEffect(()=>{
     fetch("http://localhost:8080/questions")
@@ -19,20 +20,41 @@ function App() {
       if (preguntaIndex !== preguntas.length - 1){
         setPreguntaIndex(preguntaIndex + 1)
       }
+
+      let answer = document.querySelector('input[name="option"]:checked').value;
+      if (answer) {
+        let newRespuestas = [...respuestas]
+        newRespuestas[preguntaIndex] = answer
+        setRespuestas(newRespuestas)
+      }
+
+      //TODO: enviar al server con POST
+      // fetch(url, {
+      //   method: 'POST',
+      //   body: JSON.stringify(data)})
     }
+
+    
+
 
   return (
     <div className="App">
-        <form>
-            <p>{preguntas ? 
-                preguntas[preguntaIndex].text :
-                "Loading..."
-              }
-            </p>
-            <select>
-              <option>SI</option>
-              <option>NO</option>
-            </select>
+        <p>{preguntas ? 
+            preguntas[preguntaIndex].text :
+            "Loading..."
+          }
+        </p>
+
+        <form id='form1'>
+            <label>
+              SI
+              <input type="radio" value="si" name='option' />
+            </label>
+            <label>
+              NO
+              <input type="radio" value="no" name='option' />
+            </label>
+            
         </form>
         <button type='submit' onClick={() => goNext()}>NEXT</button>
     </div>
